79c0890f0531a49f4da8d028be819d4ef964a041
Ivan <ivanalexmarquez@gmail.com>
2021-09-14 11:34:52 -0400

    first step + react icon

---
+++ src/Review.js
@@ -0,0 +1,33 @@
+import React,{useState} from 'react';
+import people from './data';
+import {FaQuoteRight, FaChevronLeft, FaChevronRight} from 'react-icons/fa';
+
+const Review = () => {
+    const [index,setIndex]=useState(0);
+    //aplica destructuring de arreglo en el indice "index"
+    const {name,job,image,text}=people[index];
+    return (
+        <article className="review">
+            <div className="img-container">
+                <img src={image} alt={name} className="person-img" />
+                <span className="quote-icon">
+                    <FaQuoteRight/>
+                </span>
+            </div>
+            <h4 className="author">{name}</h4>
+            <p className="job">{job}</p>
+            <p className="info">{text}</p>
+        <div className="button-container">
+            <button className="prev-btn">
+                <FaChevronLeft/>
+            </button>
+            <button className="next-btn">
+                <FaChevronRight/>
+            </button>
+        </div>
+        <button className="random-btn">suprise me</button>
+        </article>
+    );
+}
+ 
+export default Review;
